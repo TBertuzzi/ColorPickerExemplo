27b6c2bac5c4a5fc0da772f3b0263dbbad293d42
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2020-04-14 09:52:49 -0300

    :tada: Subindo Exemplo para o Github

---
+++ ColorPickerExemplo/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amporis.Xamarin.Forms.ColorPicker;
+using Xamarin.Forms;
+
+namespace ColorPickerExemplo
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = this;
+        }
+
+        private Color editedColor = Color.FromHex("#FFFF8000");
+
+        public Color EditedColor
+        {
+            get => editedColor;
+            set { editedColor = value; OnPropertyChanged(); }
+        }
+
+        async void popup_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var color = await ColorPickerDialog.Show(principal, "ColorPickerDialog", Color.White, null);
+        }
+    }
+}
